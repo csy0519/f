14081d2ac84df0d55a77f9423cc51982d22dd636
csy0519 <48037681+csy0519@users.noreply.github.com>
2024-09-17 16:33:34 +0800

    Add files via upload

---
+++ globals.c
@@ -0,0 +1,9 @@
+// globals.c
+#include "globals.h"
+
+int debug_mode = 0;
+int debug_mode_SIGN=0;
+uint64_t temp_xu=0; 
+uint64_t temp_yu=0; 
+uint64_t temp_x1y1=0; 
+uint64_t temp_x0y0=0; 
---
+++ globals.h
@@ -0,0 +1,13 @@
+// globals.h
+#include <stdint.h>
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+extern int debug_mode;  // 声明全局变量
+extern int debug_mode_SIGN;  // 声明全局变量
+extern uint64_t temp_xu; 
+extern uint64_t temp_yu; 
+extern uint64_t temp_x1y1; 
+extern uint64_t temp_x0y0; 
+
+#endif
